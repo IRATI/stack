d0956936735068689e83bab185a1978f327aeabb
Francesco <f.salvestrini@nextworks.it>
2013-05-18 15:22:50 +0200

    Rearranged test

--- librina/src/main.c
+++ librina/src/main.c
@@ -13,3 +13,12 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
+
+#include <stdlib.h>
+
+#include "librina.h"
+
+int main(int argc, char * argv[])
+{
+        exit(0);
+}
