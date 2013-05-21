4f5c5b2ec14cd55f7f12ffe53a3f8fdcb1fecbac
sandervrijders <sander.vrijders@intec.ugent.be>
2013-05-21 17:01:51 +0200

    Extended skeleton of shim-eth.c

--- net/rina/shim-eth.c
+++ net/rina/shim-eth.c
@@ -19,6 +19,46 @@
 #include "logs.h"
 #include "shim-eth.h"
 
+ipc_process_id_t shim_eth_create(const name_t *name, const ipc_config_t **config)
+{
+
+}
+
+int shim_eth_destroy(ipc_process_id_t ipc_process_id)
+{
+
+}
+
+port_id_t shim_eth_allocate_flow_request(const name_t *source, const name_t *dest, const flow_spec_t *flow_spec)
+{
+
+}
+
+nt shim_eth_allocate_flow_response(const port_id_t *port_id, const response_reason_t *response)
+{
+
+}
+
+int shim_eth_deallocate_flow(port_id_t port_id)
+{
+
+}
+
+int shim_eth_register_application(const name_t *name)
+{
+
+}
+
+int shim_eth_unregister_application(const name_t *name)
+{
+
+}
+
+int shim_eth_write_sdu(port_id_t port_id, sdu_t *sdu)
+{
+
+}
+
 int shim_eth_init(void)
 {
         LOG_DBG("init");
@@ -30,3 +70,5 @@ void shim_eth_exit(void)
 {
         LOG_DBG("exit");
 }
+
+
