0ab56e780387e120fe1376775898ddfc952338f6
Francesco Salvestrini <f.salvestrini@nextworks.it>
2013-06-03 18:48:15 +0200

    Rearranged syscalls body

--- net/rina/syscalls.c
+++ net/rina/syscalls.c
@@ -1,7 +1,8 @@
 /*
  * RINA SYSTEM CALLS 
  * 
- *	Leonardo Bergesio <leonardo.bergesio@i2cat.net>
+ *    Francesco Salvestrini <f.salvestrini@nextworks.it>
+ *    Leonardo Bergesio <leonardo.bergesio@i2cat.net>
  *     
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,78 +19,82 @@
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
+#include <linux/linkage.h>
+#include <linux/kernel.h>
 
 #define RINA_PREFIX "rina_syscalls"
 
 #include "logs.h" 
 #include "common.h" 
 #include "kipcm.h" 
-#include <linux/linkage.h>
-#include <linux/kernel.h>
 
-
-asmlinkage long sys_ipc_process_create (const name_t *name,
-				    ipc_process_id_t ipcp_id,
-				    dif_type_t type)
+asmlinkage long sys_ipc_process_create(const name_t *   name,
+                                       ipc_process_id_t ipcp_id,
+                                       dif_type_t       type)
 {
-	LOG_DBG("ENTER ipc_process_create K-IPCM system call");
-	return 0;
+        LOG_FBEGN;
+        LOG_FEXIT;
 
+	return 0;
 }
 
-asmlinkage long sys_ipc_process_configure (ipc_process_id_t ipcp_id,
-				       const ipc_process_conf_t configuration)
+asmlinkage long sys_ipc_process_configure(ipc_process_id_t ipcp_id,
+                                          const ipc_process_conf_t configuration)
 {
-	LOG_DBG("ENTER ipc_process_configure K-IPCM system call");
-	return 0;
+        LOG_FBEGN;
+        LOG_FEXIT;
 
+	return 0;
 }
 
-asmlinkage long sys_ipc_process_destroy (ipc_process_id_t ipcp_id)
+asmlinkage long sys_ipc_process_destroy(ipc_process_id_t ipcp_id)
 {
-	LOG_DBG("ENTER ipc_process_destroy K-IPCM system call");
-	return 0;
+        LOG_FBEGN;
+        LOG_FEXIT;
 
+	return 0;
 }
 
-asmlinkage long sys_read_sdu (port_id_t port_id,
-			      bool_t block,
-			      sdu_t *sdu)
+asmlinkage long sys_read_sdu(port_id_t port_id,
+                             bool_t block,
+                             sdu_t *sdu)
 {
-	LOG_DBG("ENTER readu_sdu K-IPCM system call");
-	return 0;
+        LOG_FBEGN;
+        LOG_FEXIT;
 
+	return 0;
 }
 
-asmlinkage long sys_write_sdu (port_id_t port_id,
-			   const sdu_t *sdu)
+asmlinkage long sys_write_sdu(port_id_t port_id,
+                              const sdu_t *sdu)
 {
-	LOG_DBG("ENTER write_sdu K-IPCM system call");
-	return 0;
+        LOG_FBEGN;
+        LOG_FEXIT;
 
+	return 0;
 }
 
-asmlinkage long sys_efcp_create (const connection_t *connection)
+asmlinkage long sys_efcp_create(const connection_t *connection)
 {
-	LOG_DBG("ENTER efcp_create EFCP system call");
-	return 0;
+        LOG_FBEGN;
+        LOG_FEXIT;
 
+	return 0;
 }
 
-asmlinkage long sys_efcp_destroy (cep_id_t cep_id)
+asmlinkage long sys_efcp_destroy(cep_id_t cep_id)
 {
-	LOG_DBG("ENTER efcp_destroy EFCP system call");
-	return 0;
+        LOG_FBEGN;
+        LOG_FEXIT;
 
+	return 0;
 }
 
-asmlinkage long sys_efcp_update (cep_id_t cep_id,
-				 cep_id_t dest_cep_id)
+asmlinkage long sys_efcp_update(cep_id_t cep_id,
+                                cep_id_t dest_cep_id)
 {
-	LOG_DBG("ENTER efcp_update EFCP system call");
+        LOG_FBEGN;
+        LOG_FEXIT;
+
 	return 0;
-
 }
-
-
-
