0d554b543273271ad9fa19ac3dc34064ee720bc2
Leonardo Bergesio <leonardo.bergesio@i2cat.net>
2013-06-06 12:18:41 -0400

    nl module __init and __exit and extras

--- net/rina/netlink.c
+++ net/rina/netlink.c
@@ -2,6 +2,7 @@
  * NetLink support
  *
  *    Francesco Salvestrini <f.salvestrini@nextworks.it>
+ *    Leonardo Bergesio <leonardo.bergesio@i2cat.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,17 +22,58 @@
 #define RINA_PREFIX "netlink"
 
 #include "logs.h"
+#include "netlink.h"
 
 int rina_netlink_init(void)
 {
-        LOG_FBEGN;
-        LOG_FEXIT;
-
-        return 0;
+	int ret;
+	
+	LOG_FBEGN;
+	
+	ret = genl_register_family(&nl_rina_family);
+	if (ret != 0){
+		LOG_DBG("Not able to register nl_rina_family");
+		return -1;
+	}
+	
+	ret = genl_register_ops(&nl_rina_family, &nl_rina_ops_echo);
+	if (ret != 0){
+		LOG_DBG("Not able to register nl_rina_ops");
+		genl_unrigester_family(&nl_rina_family);
+		return -2;
+	}
+	LOG_FEXIT;
+	return 0;
 }
 
 void rina_netlink_exit(void)
 {
         LOG_FBEGN;
-        LOG_FEXIT;
+	
+	int ret;
+
+	/*unregister the functions*/
+	ret = genl_unregister_ops(&nl_rina_family, nl_rina_ops_echo);
+
+	if(ret != 0){
+		LOG_DBG("unregister ops: %i\n",ret);
+		return;
+	}
+	
+	/*unregister the family*/
+	ret = genl_unregister_family(&nl_rina_family);
+	
+	if(ret !=0){
+		LOG_DBG("unregister family %i\n",ret);
+	}
+	LOG_FEXIT;
 }
+
+/* handler */
+static int nl_rina_echo(struct sk_buff *skb, struct genl_info *info)
+{
+	/* message handling code goes here; return 0 on success, negative
+	* values on failure */
+	LOG_DBG("ECHOING MESSAGE");
+}
+
--- net/rina/netlink.h
+++ net/rina/netlink.h
@@ -2,6 +2,7 @@
  * NetLink support
  *
  *    Francesco Salvestrini <f.salvestrini@nextworks.it>
+ *    Leonardo Bergesio <leonardo.bergesio@i2cat.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,5 +19,62 @@
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
+#ifndef RINA_NETLINK_SUPPORT
+#define RINA_NETLINK_SUPPORT
+
+#include <linux/module.h>
+#include <net/sock.h>
+#include <linux/netlink.h>
+#include <linux/genetlink.h>
+#include <linux/skbuff.h>
+
+#define NETLINK_RINA 31
+
+/* attributes */
+enum {
+	NETLINL_RINA_A_UNSPEC,
+	NETLINK_RINA_A_MSG,
+	__NETLINK_RINA_A_MAX,
+};
+
+#define NETLINK_RINA_A_MAX (__NETLINK_RINA_A_MAX - 1)
+
+/* attribute policy */
+static struct nla_policy nl_rina_policy[NETLINK_RINA_A_MAX + 1] = { 
+	[NETLINK_RINA_A_MSG] = { .type = NLA_NUL_STRING },
+};
+
+/* family definition */
+static struct genl_family nl_rina_family = { 
+	.id = GENL_ID_GENERATE,
+	.hdrsize = 0,
+	.name = "NETLINK_RINA",
+	.version = 1,
+	.maxattr = NETLINK_RINA_A_MAX,
+};
+
+/* handler */
+static int nl_rina_echo(struct sk_buff *skb, struct genl_info *info);
+
+/* commands */
+enum {
+	NETLINK_RINA_C_UNSPEC,
+	NETLINK_RINA_C_ECHO,
+	__NETLINK_RINA_C_MAX,
+};
+
+#define NETLINK_RINA_C_MAX (__NETLINK_RINA_C_MAX - 1)
+/* operation definition */
+static struct genl_ops nl_rina_ops_echo = {
+	.cmd = NETLINK_RINA_C_ECHO,
+	.flags = 0,
+	.policy = net_rina_policy,
+	.doit = nl_rina_echo,
+	.dumpit = NULL,
+};
+
+
 int  rina_netlink_init(void);
 void rina_netlink_exit(void);
+
+#endif
