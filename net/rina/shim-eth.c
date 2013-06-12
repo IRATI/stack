156f7a2899f49917e5cd2f001cdbb8b74b0a7bfb
Sander Vrijders <sander.vrijders@intec.ugent.be>
2013-06-12 17:41:20 +0200

    Removed shim-eth.h

--- net/rina/shim-eth.c
+++ net/rina/shim-eth.c
@@ -19,18 +19,71 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
+#define RINA_PREFIX "shim-eth"
 
 #include <linux/module.h>
 #include <linux/if_ether.h>
 #include <linux/string.h>
-
-#define RINA_PREFIX "shim-eth"
+#include <linux/list.h>
 
 #include "logs.h"
-/* FIXME: To be removed */
-#include "shim-eth.h"
+#include "common.h"
+#include "shim.h"
 
 LIST_HEAD(shim_eth);
+
+/* Holds the configuration of one shim IPC process */
+struct shim_eth_info_t {
+	struct name_t * name;
+	uint16_t        vlan_id;
+	char *      interface_name;
+};
+
+enum port_id_state_t {
+	PORT_STATE_NULL,
+	PORT_STATE_RECIPIENT_ALLOCATE_PENDING,
+	PORT_STATE_INITIATOR_ALLOCATE_PENDING,
+	PORT_STATE_ALLOCATED
+};
+
+/* Hold the information related to one flow*/
+struct shim_eth_flow_t {
+	uint64_t             src_mac;
+	uint64_t             dst_mac;
+	port_id_t            port_id;
+	enum port_id_state_t port_id_state;
+	/* FIXME: Will probably be a sk_buff_head or a linked list 
+	   holding only the SDUs */
+
+        /* QUEUE(sdu_queue, sdu_t *); */
+};
+
+/*
+ * Contains all the information associated to an instance of a
+ * shim Ethernet IPC Process, as well as pointers to the
+ * functions to manipulate it
+ */
+struct shim_eth_instance_t {
+        /* The configuration of the shim IPC Process */
+	struct shim_eth_info_t * info;
+
+	/* FIXME: Pointer to the device driver data_structure */
+	/* device_t * device_driver; */
+
+	/* FIXME: Stores the state of flows indexed by port_id */
+	/* HASH_TABLE(flow_to_port_id, port_id_t, shim_eth_flow_t); */
+};
+
+/*
+ * The container for the shim IPC Process over Ethernet
+ * component
+ */
+/* Stores the state of shim IPC Process instances */
+struct shim_eth_t {
+	ipc_process_id_t ipc_process_id;
+	struct shim_eth_instance_t * shim_eth_instance;
+	struct list_head list;
+};
 
 int shim_eth_init(void)
 {
