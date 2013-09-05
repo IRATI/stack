3ba87ff13ff20d4240e8656196ac346d0a93f6d2
Francesco Salvestrini <f.salvestrini@nextworks.it>
2013-09-05 13:11:15 +0200

    Added few logs (debugging)

--- linux/net/rina/ipcp-utils.c
+++ linux/net/rina/ipcp-utils.c
@@ -130,6 +130,8 @@ void name_fini(struct name * n)
                 rkfree(n->entity_instance);
                 n->entity_instance = NULL;
         }
+
+        LOG_DBG("Name at %pK finalized successfully", n);
 }
 EXPORT_SYMBOL(name_fini);
 
@@ -142,6 +144,8 @@ void name_destroy(struct name * ptr)
         ASSERT(name_is_initialized(ptr));
 
         rkfree(ptr);
+
+        LOG_DBG("Name at %pK destroyed successfully", ptr);
 }
 EXPORT_SYMBOL(name_destroy);
 
