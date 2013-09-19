ec997f4d07ad5b773ca9c5abd77eaa514a241424
Francesco Salvestrini <f.salvestrini@nextworks.it>
2013-09-19 18:30:49 +0200

    Added new RINA ARP stub

---
+++ linux/net/rina/rina-arp-new.h
@@ -0,0 +1,74 @@
+/*
+ * Implementation of RFC 826 because current implementation is too
+ * intertwined with IP version 4.
+ *
+ *    Sander Vrijders       <sander.vrijders@intec.ugent.be>
+ *    Francesco Salvestrini <f.salvestrini@nextworks.it>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#ifndef RINA_ARP_H
+#define RINA_ARP_H
+
+#include <linux/types.h>
+#include <linux/kernel.h>
+#include <linux/netdevice.h>
+#include <linux/if_ether.h>
+
+struct naddr_handle;
+struct naddr_filter;
+
+struct paddr {
+	void * buf;
+	size_t length;
+};
+
+enum rinarp_mac_addr_type {
+        MAC_ADDR_802_3
+};
+
+struct rinarp_mac_addr {
+        enum rinarp_mac_addr_type type;
+        union {
+                uint8_t mac_802_3[6];
+        } data;
+};
+
+typedef int hwaddr_t; /* FIXME: Sander fix this */
+
+struct naddr_handle * rinarp_paddr_register(__be16              proto_name,
+					    __be16              proto_len,
+                                            struct net_device * device,
+                                            struct paddr        address);
+int                   rinarp_paddr_unregister(struct naddr_handle * h);
+
+typedef void (* arp_handler_t)(const struct paddr *           dest_net_addr,
+                               const struct rinarp_mac_addr * dest_hw_addr);
+
+struct naddr_filter * naddr_filter_create(struct naddr_handle * handle);
+int                   naddr_filter_set(struct naddr_filter * filter,
+                                       arp_handler_t         request,
+                                       arp_handler_t         reply);
+int                   naddr_filter_destroy(struct naddr_filter * filter);
+           
+int                   rinarp_hwaddr_get(struct naddr_filter * filter, 
+                                        struct paddr          in_address,
+                                        hwaddr_t *            out_addr);
+
+int                   rinarp_send_request(struct naddr_filter * filter, 
+                                          struct paddr          address);
+
+#endif
