dc5ebb861a42cd16ccffdc45da686b638bad5228
Francesco Salvestrini <f.salvestrini@nextworks.it>
2013-09-23 15:16:47 +0200

    Added FMGR component

---
+++ linux/net/rina/fmgr.c
@@ -0,0 +1,54 @@
+/*
+ * FMGR (Flows Manager)
+ *
+ *    Francesco Salvestrini <f.salvestrini@nextworks.it>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#define RINA_PREFIX "fmgr"
+
+#include "logs.h"
+#include "debug.h"
+#include "utils.h"
+#include "fidm.h"
+
+struct fmgr {
+        int keep_me;
+};
+
+struct fmgr * fmgr_create(void)
+{
+        struct fmgr * tmp;
+
+        tmp = rkzalloc(sizeof(*tmp), GFP_KERNEL);
+        if (!tmp)
+                return NULL;
+
+        return tmp;
+}
+
+int fmgr_destroy(struct fmgr * instance)
+{
+        if (!instance) {
+                LOG_ERR("Bogus instance passed, bailing out");
+                return -1;
+        }
+
+        rkfree(instance);
+        return 0;
+}
+
+
