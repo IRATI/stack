f6121940eef3ea8227348fcc122608877682b14c
Leonardo Bergesio <leonardo.bergesio@i2cat.net>
2013-10-02 16:40:10 +0100

    kipcm maps renamed

--- linux/net/rina/kipcm-utils.h
+++ linux/net/rina/kipcm-utils.h
@@ -26,8 +26,8 @@
 #include "fidm.h"
 
 struct ipcp_imap;
-struct seqn_fmap;
-struct fid_snmap;
+struct kipcm_fmap;
+struct kipcm_smap;
 
 struct ipcp_imap *     ipcp_imap_create(void);
 int                    ipcp_imap_destroy(struct ipcp_imap * map);
@@ -44,31 +44,31 @@ int                    ipcp_imap_update(struct ipcp_imap *     map,
 int                    ipcp_imap_remove(struct ipcp_imap * map,
                                         ipc_process_id_t   key);
 
-struct seqn_fmap *     seqn_fmap_create(void);
-int                    seqn_fmap_destroy(struct seqn_fmap * map);
-int                    seqn_fmap_empty(struct seqn_fmap * map);
-rnl_sn_t               seqn_fmap_find(struct seqn_fmap * map,
-                                      flow_id_t          key);
-int                    seqn_fmap_update(struct seqn_fmap * map,
-                                        flow_id_t          key,
-                                        rnl_sn_t           value);
-int                    seqn_fmap_add(struct seqn_fmap * map,
-                                     flow_id_t          key,
-                                     rnl_sn_t           value);
-int                    seqn_fmap_remove(struct seqn_fmap * map,
-                                        flow_id_t          key);
-struct fid_snmap *     fid_snmap_create(void);
-int                    fid_snmap_destroy(struct fid_snmap * map);
-int                    fid_snmap_empty(struct fid_snmap * map);
-struct flow_id_t *     fid_snmap_find(struct fid_snmap * map,
-                                      rnl_sn_t           key);
-int                    fid_snmap_update(struct fid_snmap *  map,
-                                        rnl_sn_t            key,
-                                        flow_id_t *         value);
-int                    fid_snmap_add(struct fid_snmap *  map,
-                                     rnl_sn_t            key,
-                                     flow_id_t *         value);
-int                    fid_snmap_remove(struct fid_snmap * map,
-                                        rnl_sn_t           key);
+struct kipcm_fmap *    kipcm_fmap_create(void);
+int                    kipcm_fmap_destroy(struct kipcm_fmap * map);
+int                    kipcm_fmap_empty(struct kipcm_fmap * map);
+rnl_sn_t               kipcm_fmap_find(struct kipcm_fmap * map,
+                                       flow_id_t           key);
+int                    kipcm_fmap_update(struct kipcm_fmap * map,
+                                         flow_id_t           key,
+                                         rnl_sn_t            value);
+int                    kipcm_fmap_add(struct kipcm_fmap * map,
+                                      flow_id_t           key,
+                                      rnl_sn_t            value);
+int                    kipcm_fmap_remove(struct kipcm_fmap * map,
+                                         flow_id_t           key);
+struct kipcm_smap *    kipcm_smap_create(void);
+int                    kipcm_smap_destroy(struct kipcm_smap * map);
+int                    kipcm_smap_empty(struct kipcm_smap * map);
+flow_id_t *            kipcm_smap_find(struct kipcm_smap * map,
+                                       rnl_sn_t            key);
+int                    kipcm_smap_update(struct kipcm_smap * map,
+                                         rnl_sn_t            key,
+                                         flow_id_t *         value);
+int                    kipcm_smap_add(struct kipcm_smap * map,
+                                      rnl_sn_t            key,
+                                      flow_id_t *         value);
+int                    kipcm_smap_remove(struct kipcm_smap * map,
+                                         rnl_sn_t            key);
 
 #endif
