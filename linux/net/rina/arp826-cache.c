e47af687909b182eea5a6bf9e2a7665ed385d074
Francesco Salvestrini <f.salvestrini@nextworks.it>
2013-10-08 10:28:18 +0200

    Added ARP-826 cache placeholder

---
+++ linux/net/rina/arp826-cache.c
@@ -0,0 +1,43 @@
+/*
+ * ARP 826 cache
+ *
+ *    Francesco Salvestrini <f.salvestrini@nextworks.it>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/*
+ * FIXME: The following lines provide basic framework and utilities. These
+ *        dependencies will be removed ASAP to let this module live its own
+ *        life
+ */
+#define RINA_PREFIX "arp826-cache"
+
+#include "logs.h"
+#include "debug.h"
+#include "utils.h"
+/* FIXME: End of dependencies ... */
+
+static struct list_head cache;
+
+int arp826_cache_init(void)
+{
+        INIT_LIST_HEAD(&cache);
+
+        return 0;
+}
+
+void arp826_cache_fini(void)
+{ }
