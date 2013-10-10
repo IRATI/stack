e53044bae486c9f57196ee212a72116d5d84973f
Eduard Grasa <eduard.grasa@i2cat.net>
2013-10-10 19:36:48 +0200

    Modified .gitignore and added untracked file, fixes #82

---
+++ rinad/src/ipcmanager/src/main/java/rina/ipcmanager/impl/console/AssignToDIFCommand.java
@@ -0,0 +1,38 @@
+package rina.ipcmanager.impl.console;
+
+import rina.ipcmanager.impl.IPCManager;
+
+/**
+ * The command to create a new IPC Process
+ * @author eduardgrasa
+ *
+ */
+public class AssignToDIFCommand extends ConsoleCommand{
+
+	public static final String ID = "assigndif";
+	private static final String USAGE = "assigndif <ipcp_id> <dif_name>";
+	
+	private long ipcProcessId;
+	private String difName;
+	
+	public AssignToDIFCommand(IPCManager ipcManager){
+		super(ID, ipcManager);
+	}
+	
+	@Override
+	public String execute(String[] splittedCommand) {
+		if (splittedCommand.length != 3){
+			return "Wrong number of parameters. Usage: "+USAGE;
+		}
+		
+		try{
+			ipcProcessId = Long.parseLong(splittedCommand[1]);
+			difName = splittedCommand[2];
+			getIPCManager().assignToDIF(ipcProcessId, difName);
+			return "IPC Process " + ipcProcessId + " successfully assigned to DIF "+difName;
+		}catch(Exception ex){
+			return "Error executing assign IPC Process to DIF command: " + ex.getMessage();
+		}
+	}
+
+}
