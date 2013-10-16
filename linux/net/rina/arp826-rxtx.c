9f25930c3ee144f24a45a4b718d48eccf931996f
Francesco Salvestrini <f.salvestrini@nextworks.it>
2013-10-16 10:55:14 +0200

    Added initial placeholder for ARM826 RX/TX

---
+++ linux/net/rina/arp826-rxtx.c
@@ -0,0 +1,27 @@
+/*
+ * ARP 826 (wonnabe) RX/TX functions
+ *
+ *    Francesco Salvestrini <f.salvestrini@nextworks.it>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/* FIXME: The following dependencies have to be removed */
+#define RINA_PREFIX "arp826-rxtx"
+
+#include "logs.h"
+#include "debug.h"
+#include "utils.h"
+/* FIXME: End of dependencies ... */
