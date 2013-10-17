28c6de356904466eb336e6defbf58207e6725570
Francesco Salvestrini <f.salvestrini@nextworks.it>
2013-10-17 16:28:59 +0200

    Moved rinarp.py in the tests root directory

--- linux/net/rina/arp826-core.c
+++ linux/net/rina/arp826-core.c
@@ -69,6 +69,74 @@ static void protocol_remove(uint16_t ptype)
         tbls_destroy(ptype);
 }
 
+#define CONFIG_ARP826_REGRESSION_TESTS
+
+static int regression_tests(void)
+{
+        struct gpa * a;
+        uint8_t      name_tmp[] = { 0x01, 0x02, 0x03, 0x04 };
+        struct gpa * b;
+        size_t       len_a_1, len_a_2;
+        size_t       len_b_1, len_b_2;
+
+        LOG_DBG("Regression test #1.1");
+        a = gpa_create(name_tmp, sizeof(name_tmp));
+        if (!a)
+                return -1;
+        len_a_1 = gpa_address_length(a);
+
+        LOG_DBG("Regression test #1.2");
+        b = gpa_create(name_tmp, sizeof(name_tmp));
+        if (!b)
+                return -1;
+        len_b_1 = gpa_address_length(b);
+
+        LOG_DBG("Regression test #2");
+        if (!gpa_is_equal(a, b))
+                return -1;
+        if (len_a_1 != len_b_1)
+                return -1;
+
+        LOG_DBG("Regression test #3.1");
+        if (gpa_address_grow(a, sizeof(name_tmp) * 2, 0xff))
+                return -1;
+        len_a_2 = gpa_address_length(a);
+
+        if (gpa_address_grow(b, sizeof(name_tmp) * 2, 0xff))
+                return -1;
+        len_b_2 = gpa_address_length(b);
+
+        if (!gpa_is_equal(a, b))
+                return -1;
+
+        LOG_DBG("Regression test #3.2");
+        if (len_a_2 != len_b_2)
+                return -1;
+
+        LOG_DBG("Regression test #3.3");
+        if ((len_a_1 == len_a_2) ||
+            (len_b_1 == len_b_2))
+                return -1;
+
+        LOG_DBG("Regression test #4.1");
+        if (gpa_address_shrink(a, 0xff))
+                return -1;
+        if (gpa_address_shrink(b, 0xff))
+                return -1;
+
+        LOG_DBG("Regression test #4.2");
+        if (gpa_address_length(a) != len_a_1)
+                return -1;
+        LOG_DBG("Regression test #4.3");
+        if (gpa_address_length(b) != len_b_1)
+                return -1;
+
+        gpa_destroy(b);
+        gpa_destroy(a);
+
+        return 0;
+}
+
 static int __init mod_init(void)
 {
         LOG_DBG("Initializing");
@@ -81,6 +149,12 @@ static int __init mod_init(void)
                 return -1;
         }
 
+#ifdef CONFIG_ARP826_REGRESSION_TESTS
+        if (regression_tests()) {
+                LOG_ERR("Regression tests do not pass, bailing out");
+                return -1;
+        }
+#endif
         if (protocol_add(ETH_P_RINA, 6)) {
                 tbls_fini();
                 arm_fini();
