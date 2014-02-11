69e7fa19a4b54e47c9adf2da096022fc6062857e
Francesco Salvestrini <f.salvestrini@nextworks.it>
2014-02-11 15:47:01 +0100

    Rearranged

--- linux/net/rina/rmt.c
+++ linux/net/rina/rmt.c
@@ -870,6 +870,7 @@ static int receive_worker(void * o)
                         case PDU_TYPE_MGMT:
                                 process_mgmt_sdu(tmp, port_id, sdu);
                                 break;
+
                         case PDU_TYPE_EFCP:
                         case PDU_TYPE_CC:
                         case PDU_TYPE_SACK:
@@ -886,6 +887,7 @@ static int receive_worker(void * o)
                                  */
                                 process_dt_sdu(tmp, port_id, sdu, entry);
                                 break;
+
                         default:
                                 LOG_ERR("Unknown PDU type %d", pdu_type);
                                 sdu_destroy(sdu);
