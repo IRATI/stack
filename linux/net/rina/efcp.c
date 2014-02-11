53e6d16ede2b072ec7b18f51c8a5b5b879d60074
Francesco Salvestrini <f.salvestrini@nextworks.it>
2014-02-11 12:46:16 +0100

    Added DTCP parenting in EFCP

--- linux/net/rina/efcp.c
+++ linux/net/rina/efcp.c
@@ -533,7 +533,7 @@ cep_id_t efcp_connection_create(struct efcp_container * container,
 
         dtcp = NULL;
         if (connection->policies_params.dtcp_present) {
-                dtcp = dtcp_create();
+                dtcp = dtcp_create(tmp->dt);
                 if (!dtcp) {
                         dtp_destroy(dtp);
                         efcp_destroy(tmp);
