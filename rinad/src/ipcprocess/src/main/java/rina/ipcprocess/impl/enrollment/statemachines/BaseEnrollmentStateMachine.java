77c91c577f810abd8888e9f52c331246c7ae297c
Eduard Grasa <eduard.grasa@i2cat.net>
2014-02-19 16:48:48 +0100

    Fixed bug in dead neighbor detection, and made number of re-enrollment attempts configurable

--- rinad/src/ipcprocess/src/main/java/rina/ipcprocess/impl/enrollment/statemachines/BaseEnrollmentStateMachine.java
+++ rinad/src/ipcprocess/src/main/java/rina/ipcprocess/impl/enrollment/statemachines/BaseEnrollmentStateMachine.java
@@ -280,11 +280,13 @@ public abstract class BaseEnrollmentStateMachine extends BaseCDAPMessageHandler{
 		//Create or update the neighbor information in the RIB
 		try{
 			remotePeer.setEnrolled(enrolled);
+			remotePeer.setNumberOfEnrollmentAttempts(0);
 			if (enrolled){
 				remotePeer.setUnderlyingPortId(this.getPortId());
 			}else{
 				remotePeer.setUnderlyingPortId(0);
 			}
+			
 			ribDaemon.create(NeighborRIBObject.NEIGHBOR_RIB_OBJECT_CLASS, 
 					NeighborSetRIBObject.NEIGHBOR_SET_RIB_OBJECT_NAME + RIBObjectNames.SEPARATOR 
 						+ remotePeer.getName().getProcessName(), 
