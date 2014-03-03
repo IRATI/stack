da4a06d10229181df611159c6a44e7a92d419d3f
Francesco Salvestrini <f.salvestrini@nextworks.it>
2014-03-03 11:39:36 +0100

    Fixed spin_lock/spin_unlock

--- linux/net/rina/ipcps/normal-ipcp.c
+++ linux/net/rina/ipcps/normal-ipcp.c
@@ -491,6 +491,7 @@ static int normal_mgmt_sdu_read(struct ipcp_instance_data * data,
                 spin_unlock(&mgmt_data->lock);
                 return -1;
         }
+
         atomic_inc(&mgmt_data->readers);
         while (rfifo_is_empty(mgmt_data->sdu_ready)) {
                 LOG_DBG("Mgmt read going to sleep...");
@@ -499,14 +500,14 @@ static int normal_mgmt_sdu_read(struct ipcp_instance_data * data,
                 retval = wait_event_interruptible(mgmt_data->wait_q,
                                                   queue_ready(mgmt_data));
 
+                spin_lock(&mgmt_data->lock);
                 if (!mgmt_data  || !mgmt_data->sdu_ready) {
                         LOG_ERR("No mgmt data anymore, waitqueue "
                                 "return code was %d", retval);
-                        spin_unlock(&data->mgmt_data->lock);
+                        spin_unlock(&mgmt_data->lock);
                         return -1;
                 }
 
-                spin_lock(&mgmt_data->lock);
                 if (retval) {
                         LOG_DBG("Mgmt queue waken up by interruption, "
                                 "returned error %d", retval);
