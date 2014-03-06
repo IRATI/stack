d07f5f74cf3ba6b989886d6f089fea4a4c17b876
Francesco Salvestrini <f.salvestrini@nextworks.it>
2014-03-06 21:21:05 +0100

    Added stubs for missing symbols (compilation closes now)

--- linux/net/rina/dt-utils.c
+++ linux/net/rina/dt-utils.c
@@ -147,7 +147,6 @@ struct rtxq * rtxq_create(void)
         return tmp;
 }
 
-
 int rtxq_destroy(struct rtxq * q)
 {
         if (!q)
@@ -157,3 +156,28 @@ int rtxq_destroy(struct rtxq * q)
 
         return 0;
 }
+
+int rtxq_push(struct rtxq *  q,
+              struct pdu *   pdu)
+{
+        if (!q || !pdu)
+                return -1;
+
+        LOG_MISSING;
+
+        return -1;
+}
+
+int rtxq_set_pop(struct rtxq *      q,
+                 seq_num_t          from,
+                 seq_num_t          to,
+                 struct list_head * p)
+{
+        if (!q)
+                return -1;
+
+        LOG_MISSING;
+
+        return -1;
+}
+
