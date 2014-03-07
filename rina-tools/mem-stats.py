7a8d78465ba14c6d5c9f8ad3ce5653718b7a1223
Vincenzo Maffione <v.maffione@nextworks.it>
2014-03-07 15:26:03 +0100

    rmem: memory stats implemented
    When CONFIG_RINA_MEMORY_STATS, the rmem module will
    keep track of the number of allocated RINA objects. The objects
    are classified using their size (approximated to a power of 2).
    The current state is logged every 2 seconds.
    
    The script rina-tools/mem-stats.py can be used to automatically
    extract and show this information.
    
    Signed-off-by: Vincenzo Maffione <v.maffione@nextworks.it>

---
+++ rina-tools/mem-stats.py
@@ -0,0 +1,66 @@
+# written for python 2.x versions
+
+import re
+import sys
+
+
+if len(sys.argv) < 2:
+    print("Usage: mem-stats.py LOG_FILE")
+    quit()
+
+file_name = sys.argv[1]
+fin = open(file_name, 'r')
+
+memstat_banner = 'MEMSTAT '
+records = []
+
+while 1:
+    line = fin.readline()
+    if line == "":
+        break;
+    m = re.search(r'' + memstat_banner + '(.*)', line)
+    if m:
+        msg = m.group(1)
+        if msg.startswith('BEG'):
+            cur = dict()
+            records.append(cur)
+            m = re.search('[0-9]+', msg)
+            if m:
+                try:
+                    cur['t'] = int(m.group(0))
+                except ValueError:
+                    pass
+        elif msg.startswith('END'):
+            pass
+        else:
+            m = re.match(r'([0-9]+) ([0-9]+)', msg)
+            if m:
+                try:
+                    idx = int(m.group(1))
+                    val = int(m.group(2))
+                    cur[idx] = val
+                except ValueError:
+                    pass
+
+fin.close();
+
+lr = None
+for r in records:
+    try:
+        t = r['t']
+        print('Time %f (seconds)' % (t/1000, ))
+        for k in r.keys():
+            if k != 't':
+                if lr:
+                    diff = str(r[k] - lr[k])
+                    if r[k] >= lr[k]:
+                        diff = '+' + diff
+                else:
+                    diff = '?'
+                print('\t(%d bytes) ==> %d items [%s]' % (pow(2, k), r[k], diff))
+        print('')
+        lr = r
+    except KeyError:
+        pass
+
+# more elaborations here
