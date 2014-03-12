5d3295f31eaecc978d6266719a3f979b0ab9c904
Francesco Salvestrini <f.salvestrini@nextworks.it>
2014-03-12 12:06:19 +0100

    Rearranged mem-stats.py, added automake conditionals, moved mem-stats.py into src directory

--- rina-tools/src/rina-mem-stats.py
+++ rina-tools/src/rina-mem-stats.py
@@ -1,23 +1,26 @@
-# written for python 2.x versions
+#
+# rina-mem-stats
+#
+# Written by: Vincenzo Maffione <v DOT maffione AT nextworks DOT it>
+#
 
 import re
 import sys
-
 
 if len(sys.argv) < 2:
     print("Usage: mem-stats.py LOG_FILE")
     quit()
 
-file_name = sys.argv[1]
-fin = open(file_name, 'r')
-
+file_name      = sys.argv[1]
+fin            = open(file_name, 'r')
 memstat_banner = 'MEMSTAT '
-records = []
+records        = []
 
-while 1:
+while True:
     line = fin.readline()
     if line == "":
         break;
+
     m = re.search(r'' + memstat_banner + '(.*)', line)
     if m:
         msg = m.group(1)
@@ -36,8 +39,8 @@ while 1:
             m = re.match(r'([0-9]+) ([0-9]+)', msg)
             if m:
                 try:
-                    idx = int(m.group(1))
-                    val = int(m.group(2))
+                    idx      = int(m.group(1))
+                    val      = int(m.group(2))
                     cur[idx] = val
                 except ValueError:
                     pass
@@ -57,7 +60,8 @@ for r in records:
                         diff = '+' + diff
                 else:
                     diff = '?'
-                print('\t(%d bytes) ==> %d items [%s]' % (pow(2, k), r[k], diff))
+                print('\t(%d bytes) ==> %d items [%s]' %
+                      (pow(2, k), r[k], diff))
         print('')
         lr = r
     except KeyError:
