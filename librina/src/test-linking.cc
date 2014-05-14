9154306fe7f053701be6f5b746a5632d3f19c123
Francesco Salvestrini <f.salvestrini@nextworks.it>
2014-05-14 10:50:59 +0200

    Added linking related test on make check target

---
+++ librina/src/test-linking.cc
@@ -0,0 +1,33 @@
+/*
+ * Linking test program
+ *
+ *    Francesco Salvestrini <f.salvestrini@nextworks.it>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include <cstdlib>
+
+#include "librina-common.h"
+
+int main(int argc, char * argv[])
+{
+        (void) argc;
+        (void) argv;
+
+        rina::initialize("test", "/tmp/test");
+
+        return EXIT_SUCCESS;
+}
