6f48d0f472122fcbcb6c3a9015cb53d1707ea907
Francesco Salvestrini <f.salvestrini@nextworks.it>
2014-05-19 14:33:16 +0200

    Removed useless method from CDAP Exception

--- librina/include/librina-cdap.h
+++ librina/include/librina-cdap.h
@@ -1,18 +1,18 @@
-/*
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- */
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
 
 #ifndef LIBRINA_CDAP_H
 #define LIBRINA_CDAP_H
@@ -114,7 +114,7 @@ class CDAPException: public Exception{
 		const char* get_error_message() const;
 		int get_result() const;
 		void set_result(int result);
-		const char* get_description() const;
+		//const char* get_description() const;
 		const CDAPMessage* get_cdap_message() const;
 		void set_cdap_message(const CDAPMessage* cdap_message);
 	private:
