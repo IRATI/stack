IRATI/stack

commit e65da250ba53bd98e6e9228006652a92bccfdce6

librina: fix getFlowInformation()

librina/src/ipc-manager.cc
@@ -661,24 +661,26 @@ std::list<FlowInformation> IPCProcess::getAllocatedFlows() {
 	return allocatedFlows;
 }
 
-FlowInformation IPCProcess::getFlowInformation(int flowPortId) {
+bool IPCProcess::getFlowInformation(int flowPortId, FlowInformation& result) {
 	std::list<FlowInformation>::const_iterator iterator;
+
 	for (iterator = allocatedFlows.begin();
 			iterator != allocatedFlows.end(); ++iterator) {
-	    if (iterator->getPortId() == flowPortId)
-	    	return *iterator;
+                if (iterator->getPortId() == flowPortId)
+                        result = *iterator;
+                return true;
 	}
 
-	throw IPCException("Unknown flow");
+        return false;
 }
 
 unsigned int IPCProcess::deallocateFlow(int flowPortId) {
 	unsigned int seqNum = 0;
 	FlowInformation flowInformation;
+        bool success;
 
-	try{
-		flowInformation = getFlowInformation(flowPortId);
-	}catch (IPCException &e) {
+	success = getFlowInformation(flowPortId, flowInformation);
+        if (!success) {
 		LOG_ERR("Could not find flow with port-id %d", flowPortId);
 		throw IpcmDeallocateFlowException("Unknown flow");
 	}
@@ -722,14 +724,15 @@ void IPCProcess::deallocateFlowResult(unsigned int sequenceNumber, bool success)
 
 FlowInformation IPCProcess::flowDeallocated(int flowPortId) {
 	FlowInformation flowInformation;
+        bool success;
 
-	try {
-		flowInformation = getFlowInformation(flowPortId);
-		allocatedFlows.remove(flowInformation);
-		return flowInformation;
-	} catch (IPCException &e) {
-		throw IpcmDeallocateFlowException(e.what());
-	}
+        success = getFlowInformation(flowPortId, flowInformation);
+        if (!success) {
+                throw IpcmDeallocateFlowException("No flow for such port-id");
+        }
+        allocatedFlows.remove(flowInformation);
+
+        return flowInformation;
 }
 
 unsigned int IPCProcess::queryRIB(const std::string& objectClass,

librina/include/librina/ipc-manager.h
@@ -621,10 +621,11 @@ public:
 	std::list<FlowInformation> getAllocatedFlows();
 
 	/**
-	 * Returns the information of the flow identified by portId
-	 * @throws IPCException if no flow with the requested portId is found
+	 * Get the information of the flow identified by portId
+         * @result will contain the flow identified by portId, if any
+	 * @return true if the flow is found, false otherwise
 	 */
-	FlowInformation getFlowInformation(int portId);
+	bool getFlowInformation(int portId, FlowInformation& result);
 
 	/**
 	 * Tell the IPC Process to deallocate a flow

rinad/src/common/helpers.cc
@@ -100,19 +100,11 @@ lookup_ipcp_by_port(unsigned int port_id)
 {
         const vector<rina::IPCProcess *>& ipcps =
                 rina::ipcProcessFactory->listIPCProcesses();
+        rina::FlowInformation info;
 
         for (unsigned int i = 0; i < ipcps.size(); i++) {
-                /* TODO This is **horrible** - librina needs another
-                 * way to lookup the IPC process from a port-id
-                 * without generating exceptions.
-                 * getFlowInformation should return a boolean, indicating
-                 * the success/failure, and the result should be passed
-                 * by a reference argument. */
-                try {
-                        ipcps[i]->getFlowInformation(port_id);
+                if (ipcps[i]->getFlowInformation(port_id, info)) {
                         return ipcps[i];
-                } catch (rina::IPCException) {
-                        continue;
                 }
         }
 

commit b66adfb71f74fc1fa833e206f700c11cfe351d18

librina: fix bug introduced by e65da250ba53bd98e6e9228006652a92bccfdce6

librina/src/ipc-manager.cc
@@ -666,9 +666,10 @@ bool IPCProcess::getFlowInformation(int flowPortId, FlowInformation& result) {
 
 	for (iterator = allocatedFlows.begin();
 			iterator != allocatedFlows.end(); ++iterator) {
-                if (iterator->getPortId() == flowPortId)
+                if (iterator->getPortId() == flowPortId) {
                         result = *iterator;
-                return true;
+                        return true;
+                }
 	}
 
         return false;