7e65e13fc580d545cac3127ccd9ff718cc714a7f
Leonardo Bergesio <leonardo.bergesio@i2cat.net>
2014-07-30 15:51:36 +0200

    dbg messages writing seq_num as uints

--- linux/net/rina/dtp.c
+++ linux/net/rina/dtp.c
@@ -727,8 +727,8 @@ static seq_num_t process_A_expiration(struct dtp * dtp, struct dtcp * dtcp)
         LOG_DBG("Processing A timer expiration");
 
         LWE = dt_sv_rcv_lft_win(dt);
-        LOG_DBG("LWEU: Original LWE = %d", LWE);
-        LOG_DBG("LWEU: MAX GAPS     = %d", max_sdu_gap);
+        LOG_DBG("LWEU: Original LWE = %u", LWE);
+        LOG_DBG("LWEU: MAX GAPS     = %u", max_sdu_gap);
 
         spin_lock(&seqq->lock);
         list_for_each_entry_safe(pos, n, &seqq->queue->head, next) {
@@ -760,7 +760,7 @@ static seq_num_t process_A_expiration(struct dtp * dtp, struct dtcp * dtcp)
                                 return 0;
                         }
 
-                        LOG_DBG("Atimer: PDU %d posted", seq_num);
+                        LOG_DBG("Atimer: PDU %u posted", seq_num);
 
                         spin_lock(&seqq->lock);
                         LWE = dt_sv_rcv_lft_win(dt);
