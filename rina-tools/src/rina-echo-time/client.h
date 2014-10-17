8ddde5f919291c26548b44989e33a4b78af10b4f
Leonardo Bergesio <leonardo.bergesio@i2cat.net>
2014-10-17 15:46:02 +0200

    Add parameter to rina-echo-time to wait for flow deallocation. fixes #402

--- rina-tools/src/rina-echo-time/client.h
+++ rina-tools/src/rina-echo-time/client.h
@@ -1,18 +1,18 @@
 //
 // Echo Client
-// 
+//
 // Addy Bombeke <addy.bombeke@ugent.be>
-// 
+//
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation; either version 2 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
@@ -41,7 +41,8 @@ public:
                bool  registration,
                unsigned int size,
                unsigned int wait,
-               int gap);
+               int gap,
+               unsigned int dw);
                void run();
 protected:
         rina::Flow* createFlow();
@@ -60,5 +61,6 @@ private:
         unsigned int data_size;
         unsigned int wait;
         int gap_;
+        unsigned int dw_;
 };
 #endif//CLIENT_HPP
