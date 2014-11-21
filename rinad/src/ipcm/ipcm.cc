IRATI/stack

commit 294775e86bf553e70c7de1e53fd420265486c88a

Fixes #391

rinad/src/ipcm/ipcm.cc
@@ -303,6 +303,110 @@ IPCManager::list_ipcp_types(std::ostream& os)
         return 0;
 }
 
+/// DIFValidator CLASS
+DIFConfigValidator::DIFConfigValidator(const rina::DIFConfiguration &dif_config,
+		const rina::DIFInformation &dif_info, std::string type)
+		:dif_config_(dif_config), dif_info_(dif_info)
+{
+	if (type == "normal-ipc")
+			type_ = NORMAL;
+	else if (type == "shim-dummy")
+			type_ = SHIM_DUMMY;
+	else
+			type_ = SHIM_ETH;
+}
+
+bool DIFConfigValidator::validateConfigs()
+{
+	if (type_ == SHIM_ETH)
+		return validateShimEth();
+	else if(type_ == SHIM_DUMMY)
+		return validateShimDummy();
+	else
+		return validateNormal();
+}
+
+bool DIFConfigValidator::validateShimEth()
+{
+	return validateBasicDIFConfigs() && validateConfigParameters();
+}
+bool DIFConfigValidator::validateShimDummy()
+{
+	return validateBasicDIFConfigs();
+}
+bool DIFConfigValidator::validateNormal()
+{
+	return	dataTransferConstants() && qosCubes() &&
+			knownIPCProcessAddresses() && pdufTableGeneratorConfiguration();
+}
+
+bool DIFConfigValidator::validateBasicDIFConfigs()
+{
+	return !dif_info_.dif_name_.processName.empty();
+}
+
+bool DIFConfigValidator::validateConfigParameters()
+{
+	for (std::list<rina::Parameter>::const_iterator it =
+			dif_config_.parameters_.begin(); it !=
+					dif_config_.parameters_.end(); ++ it){
+		if (it->name.compare("interface-name") == 0){
+			return true;
+		}
+	}
+	return false;
+}
+
+bool DIFConfigValidator::dataTransferConstants() {
+	rina::DataTransferConstants data_trans_config = dif_config_.efcp_configuration_
+			.data_transfer_constants_;
+	return data_trans_config.address_length_ != 0 &&
+			data_trans_config.qos_id_length_ != 0 &&
+			data_trans_config.port_id_length_ != 0 &&
+			data_trans_config.cep_id_length_ != 0 &&
+			data_trans_config.sequence_number_length_ != 0 &&
+			data_trans_config.length_length_ != 0 &&
+			data_trans_config.max_pdu_size_ != 0 &&
+			data_trans_config.max_pdu_lifetime_ != 0;
+}
+
+bool DIFConfigValidator::qosCubes(){
+	bool result = dif_config_.efcp_configuration_.qos_cubes_.begin()
+			!= dif_config_.efcp_configuration_.qos_cubes_.end();
+
+	for (std::list<rina::QoSCube*>::const_iterator it = dif_config_.
+			efcp_configuration_.qos_cubes_.begin();
+			it != dif_config_.efcp_configuration_.qos_cubes_.end();
+			++it) {
+		bool temp_result =  !(*it)->name_.empty() &&
+				(*it)->id_ != 0;
+		result = result && temp_result;
+	}
+	return result;
+}
+
+bool DIFConfigValidator::knownIPCProcessAddresses()
+{
+	std::list<rina::StaticIPCProcessAddress> staticAddress =
+			dif_config_.nsm_configuration_.
+			addressing_configuration_.static_address_;
+	bool result = staticAddress.begin() != staticAddress.end();
+	for (std::list<rina::StaticIPCProcessAddress>::iterator it =
+			staticAddress.begin(); it != staticAddress.end(); ++it) {
+		bool temp_result = !it->ap_name_.empty() &&	it->address_ != 0;
+		result = result && temp_result;
+	}
+	return result;
+}
+
+bool DIFConfigValidator::pdufTableGeneratorConfiguration()
+{
+	return  dif_config_.pduft_generator_configuration_.
+			pduft_generator_policy_.name_.compare("LinkState") == 0 &&
+			dif_config_.pduft_generator_configuration_.
+			pduft_generator_policy_.version_.compare("0") == 0;
+}
+
 int
 IPCManager::assign_to_dif(rina::IPCProcess *ipcp,
                           const rina::ApplicationProcessNamingInformation&
@@ -407,6 +511,12 @@ IPCManager::assign_to_dif(rina::IPCProcess *ipcp,
                 dif_info.set_dif_type(ipcp->type);
                 dif_info.set_dif_configuration(dif_config);
 
+                // Validate the parameters
+                DIFConfigValidator validator(dif_config, dif_info,
+                		ipcp->type);
+                if(!validator.validateConfigs())
+                	throw rina::BadConfigurationException("DIF configuration validator failed");
+
                 // Invoke librina to assign the IPC process to the
                 // DIF specified by dif_info.
                 seqnum = ipcp->assignToDIF(dif_info);
@@ -423,7 +533,11 @@ IPCManager::assign_to_dif(rina::IPCProcess *ipcp,
                         ipcp->name.toString() <<
                         " to DIF " << dif_name.toString() << endl;
                 FLUSH_LOG(ERR, ss);
-        } catch (Exception) {
+        } catch (rina::BadConfigurationException &e) {
+        	LOG_ERR("DIF %s configuration failed", dif_name.toString().c_str());
+        	throw e;
+        }
+        catch (Exception) {
                 FLUSH_LOG(ERR, ss);
         }
 
@@ -587,9 +701,13 @@ IPCManager::apply_configuration()
                         continue;
                 }
 
-                ipcp = create_ipcp(cit->name, type);
-                assign_to_dif(ipcp, cit->difName);
-                register_at_difs(ipcp, cit->difsToRegisterAt);
+                try{
+                	ipcp = create_ipcp(cit->name, type);
+                	assign_to_dif(ipcp, cit->difName);
+                	register_at_difs(ipcp, cit->difsToRegisterAt);
+                }catch(Exception &e){
+                	LOG_ERR("Exception while applying configuration: %s", e.what());
+                }
 
                 ipcps.push_back(ipcp);
         }

rinad/src/ipcm/ipcm.h
@@ -181,6 +181,32 @@ class IPCManager : public EventLoopData {
         IPCMConsole *console;
 };
 
+class DIFConfigValidator {
+public:
+	enum Types{
+		SHIM_ETH,
+		NORMAL,
+		SHIM_DUMMY
+	};
+	DIFConfigValidator(const rina::DIFConfiguration &dif_config,
+			const rina::DIFInformation &dif_info, std::string type);
+	bool validateConfigs();
+private:
+	Types type_;
+	const rina::DIFConfiguration &dif_config_;
+    const rina::DIFInformation &dif_info_;
+
+	bool validateShimEth();
+	bool validateShimDummy();
+	bool validateNormal();
+	bool validateBasicDIFConfigs();
+	bool validateConfigParameters();
+	bool dataTransferConstants();
+	bool qosCubes();
+	bool knownIPCProcessAddresses();
+	bool pdufTableGeneratorConfiguration();
+};
+
 void register_handlers_all(EventLoop& loop);
 
 }

librina/include/librina/ipc-daemons.h
@@ -153,6 +153,19 @@ public:
 };
 
 /**
+ * Thrown when there are problems in the configuration
+ */
+class BadConfigurationException: public IPCException {
+public:
+	BadConfigurationException():
+                IPCException("Problems assigning IPC Process to DIF"){
+        }
+	BadConfigurationException(const std::string& description):
+                IPCException(description){
+        }
+};
+
+/**
  * Thrown when there are problems updating a DIF configuration
  */
 class UpdateDIFConfigurationException: public IPCException {

rinad/src/ipcp/ipc-process.cc
@@ -268,7 +268,7 @@ void IPCProcessImpl::processAssignToDIFResponseEvent(const rina::AssignToDIFResp
 		state = INITIALIZED;
 
 		try {
-			rina::extendedIPCManager->assignToDIFResponse(it->second, -1);
+			rina::extendedIPCManager->assignToDIFResponse(requestEvent, -1);
 		} catch (Exception &e) {
 			LOG_ERR("Problems communicating with the IPC Manager: %s", e.what());
 		}
@@ -278,13 +278,19 @@ void IPCProcessImpl::processAssignToDIFResponseEvent(const rina::AssignToDIFResp
 
 	//TODO do stuff
 	LOG_DBG("The kernel processed successfully the Assign to DIF request");
-
-	rib_daemon->set_dif_configuration(dif_information_.dif_configuration_);
-	resource_allocator->set_dif_configuration(dif_information_.dif_configuration_);
-	namespace_manager->set_dif_configuration(dif_information_.dif_configuration_);
-	security_manager->set_dif_configuration(dif_information_.dif_configuration_);
-	flow_allocator->set_dif_configuration(dif_information_.dif_configuration_);
-	enrollment_task->set_dif_configuration(dif_information_.dif_configuration_);
+	try{
+		rib_daemon->set_dif_configuration(dif_information_.dif_configuration_);
+		resource_allocator->set_dif_configuration(dif_information_.dif_configuration_);
+		namespace_manager->set_dif_configuration(dif_information_.dif_configuration_);
+		security_manager->set_dif_configuration(dif_information_.dif_configuration_);
+		flow_allocator->set_dif_configuration(dif_information_.dif_configuration_);
+		enrollment_task->set_dif_configuration(dif_information_.dif_configuration_);
+	}
+	catch(Exception &e){
+		state = INITIALIZED;
+		LOG_ERR("Bad configutration error: %s", e.what());
+		rina::extendedIPCManager->assignToDIFResponse(requestEvent, -1);
+	}
 
 	state = ASSIGNED_TO_DIF;
 

commit 6e63f2782dea73818b974449a3e67e99dce30bc9

ipcm: fix shim-hv validation, broken by 294775e8

rinad/src/ipcm/ipcm.cc
@@ -315,31 +315,52 @@ DIFConfigValidator::DIFConfigValidator(const rina::DIFConfiguration &dif_config,
                 :dif_config_(dif_config), dif_info_(dif_info)
 {
         if (type == "normal-ipc")
-                        type_ = NORMAL;
+                type_ = NORMAL;
         else if (type == "shim-dummy")
-                        type_ = SHIM_DUMMY;
+                type_ = SHIM_DUMMY;
+        else if (type == "shim-hv")
+                type_ = SHIM_HV;
         else
-                        type_ = SHIM_ETH;
+                type_ = SHIM_ETH;
 }
 
 bool DIFConfigValidator::validateConfigs()
 {
         if (type_ == SHIM_ETH)
                 return validateShimEth();
-        else if(type_ == SHIM_DUMMY)
+        else if (type_ == SHIM_DUMMY)
                 return validateShimDummy();
+        else if (type_ == SHIM_HV)
+                return validateShimHv();
         else
                 return validateNormal();
 }
 
 bool DIFConfigValidator::validateShimEth()
 {
-        return validateBasicDIFConfigs() && validateConfigParameters();
+        vector<string> expected_params;
+
+        expected_params.push_back("interface-name");
+
+        return validateBasicDIFConfigs() &&
+                validateConfigParameters(expected_params);
 }
+
+bool DIFConfigValidator::validateShimHv()
+{
+        vector<string> expected_params;
+
+        expected_params.push_back("vmpi-id");
+
+        return validateBasicDIFConfigs() &&
+                validateConfigParameters(expected_params);
+}
+
 bool DIFConfigValidator::validateShimDummy()
 {
         return validateBasicDIFConfigs();
 }
+
 bool DIFConfigValidator::validateNormal()
 {
         return  dataTransferConstants() && qosCubes() &&
@@ -351,18 +372,33 @@ bool DIFConfigValidator::validateBasicDIFConfigs()
         return !dif_info_.dif_name_.processName.empty();
 }
 
-bool DIFConfigValidator::validateConfigParameters()
+bool DIFConfigValidator::validateConfigParameters(
+                        const vector<string>& expected_params)
 {
-        for (std::list<rina::Parameter>::const_iterator it =
-                     dif_config_.parameters_.begin();
-             it != dif_config_.parameters_.end();
-             ++it) {
-                if (it->name.compare("interface-name") == 0) {
-                        return true;
+        vector<bool> found_params(expected_params.size());
+
+        for (unsigned int i = 0; i < found_params.size(); i++) {
+                found_params[i] = false;
+        }
+
+        for (unsigned int i = 0; i < expected_params.size(); i++) {
+                for (std::list<rina::Parameter>::const_iterator it =
+                        dif_config_.parameters_.begin();
+                                it != dif_config_.parameters_.end(); it++) {
+                        if (it->name == expected_params[i]) {
+                                found_params[i] = true;
+                                break;
+                        }
                 }
         }
 
-        return false;
+        for (unsigned int i = 0; i < found_params.size(); i++) {
+                if (!found_params[i]) {
+                        return false;
+                }
+        }
+
+        return true;
 }
 
 bool DIFConfigValidator::dataTransferConstants() {

rinad/src/ipcm/ipcm.h
@@ -186,7 +186,8 @@ public:
 	enum Types{
 		SHIM_ETH,
 		NORMAL,
-		SHIM_DUMMY
+		SHIM_DUMMY,
+                SHIM_HV
 	};
 	DIFConfigValidator(const rina::DIFConfiguration &dif_config,
 			const rina::DIFInformation &dif_info, std::string type);
@@ -197,10 +198,12 @@ private:
     const rina::DIFInformation &dif_info_;
 
 	bool validateShimEth();
+	bool validateShimHv();
 	bool validateShimDummy();
 	bool validateNormal();
 	bool validateBasicDIFConfigs();
-	bool validateConfigParameters();
+	bool validateConfigParameters(const std::vector< std::string >&
+                                      expected_params);
 	bool dataTransferConstants();
 	bool qosCubes();
 	bool knownIPCProcessAddresses();