4edd347797582e760c8b8d8a67e860b63a408a3c
Leonardo Bergesio <leonardo.bergesio@i2cat.net>
2015-01-22 15:42:47 +0100

    rtimer moved from add_timer to mod_timer

--- linux/net/rina/rds/rtimer.c
+++ linux/net/rina/rds/rtimer.c
@@ -87,6 +87,8 @@ EXPORT_SYMBOL(rtimer_is_pending);
 static int __rtimer_start(struct rtimer * timer,
                           unsigned int    millisecs)
 {
+        int status;
+
         ASSERT(timer);
 
         /* FIXME: Crappy, rearrange */
@@ -94,9 +96,12 @@ static int __rtimer_start(struct rtimer * timer,
         timer->tl.data     = (unsigned long)           timer->data;
         timer->tl.expires  = jiffies + (millisecs * HZ) / 1000;
 
-        add_timer(&timer->tl);
+        status = mod_timer(&timer->tl, timer->tl.expires);
 
-        LOG_DBG("Timer %pK started (function = %pK, data = %pK, expires = %ld",
+
+        LOG_DBG("Previously %s Timer %pK restarted (function = %pK, data = %pK, "
+                "expires = %ld",
+                status ? "active" : "inactive",
                 timer,
                 (void *) timer->tl.function,
                 (void *) timer->tl.data,
@@ -144,39 +149,13 @@ int rtimer_stop(struct rtimer * timer)
 }
 EXPORT_SYMBOL(rtimer_stop);
 
-static int __rtimer_restart(struct rtimer * timer,
-                          unsigned int    millisecs)
-{
-        int status;
-
-        ASSERT(timer);
-
-        /* FIXME: Crappy, rearrange */
-        timer->tl.function = (void (*)(unsigned long)) timer->function;
-        timer->tl.data     = (unsigned long)           timer->data;
-        timer->tl.expires  = jiffies + (millisecs * HZ) / 1000;
-
-        status = mod_timer(&timer->tl, timer->tl.expires);
-
-
-        LOG_DBG("Previously %s Timer %pK restarted (function = %pK, data = %pK, "
-                "expires = %ld",
-                status ? "active" : "inactive",
-                timer,
-                (void *) timer->tl.function,
-                (void *) timer->tl.data,
-                timer->tl.expires);
-
-        return 0;
-}
-
 int rtimer_restart(struct rtimer * timer,
                    unsigned int    millisecs)
 {
         if (!timer)
                 return -1;
 
-        return __rtimer_restart(timer, millisecs);
+        return __rtimer_start(timer, millisecs);
 }
 EXPORT_SYMBOL(rtimer_restart);
 
