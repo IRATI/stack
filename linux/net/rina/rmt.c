0f284e83b31d1302175a943c3998374233f6557d
Eduard <eduard.grasa@i2cat.net>
2015-02-10 02:07:01 +0100

    Fixed bug in previous fix

--- linux/net/rina/rmt.c
+++ linux/net/rina/rmt.c
@@ -1073,7 +1073,7 @@ static int forward_pdu(struct rmt * rmt,
                 }
 
                 queue = n1pmap_find(rmt->ingress.n1_ports,
-                					rmt->ingress.cache.pids[i]);
+                					rmt->ingress.cache.pids[0]);
                 if (!queue)
                         /* FIXME: As in the continue before, some port could not
                          * be used */
