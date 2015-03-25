7d05d5e1e3a00e3f0651699ccc173ede9c455d58
Marc Sune <marc.sune@bisdn.de>
2015-03-25 01:05:30 +0100

    Disabling Promise() constructor

--- rinad/src/ipcm/ipcm.h
+++ rinad/src/ipcm/ipcm.h
@@ -87,9 +87,14 @@ typedef enum ipcm_res{
 class Promise {
 
 public:
+
+//This should not be necessary, and would force users to have to create
+//a new promise everytime
+#if 0
 	Promise() {
 		ret = IPCM_PENDING;
 	}
+#endif
 	virtual ~Promise(){};
 
 	//
