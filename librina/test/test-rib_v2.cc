40f2bcdfcb9f53e916e1c7920cdc4350467b592e
Marc Sune <marc.sune@bisdn.de>
2015-05-29 13:18:15 +0200

    Adding test-rib_v2.cc skeleton using libcppunit

---
+++ librina/test/test-rib_v2.cc
@@ -0,0 +1,142 @@
+//
+// RIB v2 test suite
+//
+//	  Marc Sune <marc.sune (at) bisdn.de>
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+// MA  02110-1301  USA
+//
+#include <iostream>
+#include <stdlib.h>
+
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/ui/text/TestRunner.h>
+
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "librina/cdap.h"
+#include "librina/ipc-process.h"
+#include "librina/common.h"
+#include "librina/rib_v2.h"
+
+using namespace rina;
+
+class ribBasicOps : public CppUnit::TestFixture {
+
+	CPPUNIT_TEST_SUITE( ribBasicOps );
+	CPPUNIT_TEST( testDestruction );
+/*	CPPUNIT_TEST( testCreation );
+	CPPUNIT_TEST( testAddObj );
+	CPPUNIT_TEST( testUserData );
+	CPPUNIT_TEST( testRemoveObj );
+*/
+	CPPUNIT_TEST_SUITE_END();
+
+private:
+
+
+public:
+	void setUp();
+	void tearDown();
+
+	void testDestruction();
+};
+
+//Register
+CPPUNIT_TEST_SUITE_REGISTRATION( ribBasicOps );
+
+class remoteHandlers : public rina::rib::RIBOpsRespHandlers {
+
+//
+//Required mock ups
+//
+public:
+	~remoteHandlers(){};
+
+	void remoteCreateResult(const cdap_rib::con_handle_t &con,
+			const cdap_rib::obj_info_t &obj,
+			const cdap_rib::res_info_t &res){ (void)con; (void)obj; (void)res; };
+	void remoteDeleteResult(const cdap_rib::con_handle_t &con,
+			const cdap_rib::res_info_t &res){ (void)con; (void)res; };
+	void remoteReadResult(const cdap_rib::con_handle_t &con,
+			const cdap_rib::obj_info_t &obj,
+			const cdap_rib::res_info_t &res){ (void)con; (void)obj; (void)res; };
+	void remoteCancelReadResult(const cdap_rib::con_handle_t &con,
+			const cdap_rib::res_info_t &res){ (void)con; (void)res; };
+	void remoteWriteResult(const cdap_rib::con_handle_t &con,
+			const cdap_rib::obj_info_t &obj,
+			const cdap_rib::res_info_t &res){ (void)con; (void)obj; (void)res; };
+	void remoteStartResult(const cdap_rib::con_handle_t &con,
+			const cdap_rib::obj_info_t &obj,
+			const cdap_rib::res_info_t &res){ (void)con; (void)obj; (void)res; };
+	void remoteStopResult(const cdap_rib::con_handle_t &con,
+			const cdap_rib::obj_info_t &obj,
+			const cdap_rib::res_info_t &res){ (void)con; (void)obj; (void)res; };
+};
+
+static class remoteHandlers remote_handlers;
+
+class AppHandlers : public rina::cacep::AppConHandlerInterface {
+
+public:
+	~AppHandlers(){};
+
+	/// A remote Connect request has been received.
+	void connect(int invoke_id, const cdap_rib::con_handle_t &con){ (void)con; (void)invoke_id;};
+	/// A remote Connect response has been received.
+	void connectResult(const cdap_rib::res_info_t &res,
+			const cdap_rib::con_handle_t &con){ (void)con; (void)res; };
+	/// A remote Release request has been received.
+	void release(int invoke_id, const cdap_rib::con_handle_t &con){ (void)con; (void)invoke_id; };
+	/// A remote Release response has been received.
+	void releaseResult(const cdap_rib::res_info_t &res,
+			const cdap_rib::con_handle_t &con){ (void)con; (void)res; };
+};
+
+static class AppHandlers app_handlers;
+
+//
+// End of mockups
+//
+
+void ribBasicOps::setUp(){
+	cdap_rib::cdap_params params;
+	params.is_IPCP_ = false;
+
+	rina::rib::init(&app_handlers, &remote_handlers, params);
+}
+
+void ribBasicOps::tearDown(){
+	//rina::rib::fini();
+}
+
+void ribBasicOps::testDestruction(){
+
+}
+
+int main(int args, char** argv){
+
+	(void)args;
+	(void)argv;
+
+	CppUnit::TextUi::TestRunner runner;
+	CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
+	runner.addTest( registry.makeTest() );
+	bool wasSuccessful = runner.run( "", false );
+
+	int rc = (wasSuccessful) ? EXIT_SUCCESS : EXIT_FAILURE;
+	return rc;
+}
