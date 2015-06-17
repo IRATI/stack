abf12c67872437886b14f5656e1f1a47c482e43c
Leonardo Bergesio <leonardo.bergesio@i2cat.net>
2015-06-17 16:54:40 +0200

    added structure for congestion avoidance PS

---
+++ plugins/cong_avoidance/rmt-ps-cas.c
@@ -0,0 +1,236 @@
+/*
+ * Congestion avoidance Scheme based on Jain's report PS
+ *
+ *    Leonardo Bergesio <leonardo.bergesio@i2cat.net>
+ *
+ * This program is free software; you can dummyistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include <linux/export.h>
+#include <linux/module.h>
+#include <linux/string.h>
+
+#define RINA_PREFIX "cas-rmt-ps"
+
+#include "logs.h"
+#include "debug.h"
+#include "rds/rmem.h"
+#include "rmt-ps.h"
+
+#define rmap_hash(T, K) hash_min(K, HASH_BITS(T))
+
+struct cas_rmt_queue {
+        struct rfifo *    queue;
+        unsigned int      key;
+        unsigned int      max_q;
+        unsigned int      avg_q;
+        long     int      n;
+        struct hlist_node hlist;
+};
+
+struct cas_rmt_ps_data {
+        DECLARE_HASHTABLE(queues, RMT_PS_HASHSIZE);
+};
+
+static struct cas_rmt_queue * cas_queue_create(unsigned int key)
+{
+        struct cas_rmt_queue * tmp;
+        tmp = rkzalloc(sizeof(*tmp), GFP_ATOMIC);
+        if (!tmp)
+                return NULL;
+        tmp->queue = rfifo_create_ni();
+        if (!tmp->queue) {
+                rkfree(tmp);
+                return NULL;
+        }
+
+        tmp->key = key;
+        tmp->avg_q = 0;
+        tmp->n     = 0;
+        INIT_HLIST_NODE(&tmp->hlist);
+
+        return tmp;
+}
+
+static int cas_rmt_queue_destroy(struct cas_rmt_queue * q)
+{
+        if (!q) {
+                LOG_ERR("No RMT Key-queue to destroy...");
+                return -1;
+        }
+
+        hash_del(&q->hlist);
+
+        if (q->queue) rfifo_destroy(q->queue, (void (*)(void *)) pdu_destroy);
+
+        rkfree(q);
+
+        return 0;
+}
+
+struct cas_rmt_queue * cas_rmt_queue_find(struct cas_rmt_ps_data * data,
+                                          unsigned int             key)
+{
+        struct cas_rmt_queue *    entry;
+        const struct hlist_head * head;
+
+        ASSERT(data);
+
+        head = &data->queues[rmap_hash(data->queues, key)];
+        hlist_for_each_entry(entry, head, hlist) {
+                if (entry->key == key)
+                        return entry;
+        }
+
+        return NULL;
+}
+
+
+static void cas_max_q_policy_tx(struct rmt_ps *      ps,
+                                struct pdu *         pdu,
+                                struct rmt_n1_port * port)
+{ printk("%s: called()\n", __func__); }
+
+static void cas_max_q_policy_rx(struct rmt_ps *      ps,
+                                struct sdu *         sdu,
+                                struct rmt_n1_port * port)
+{ printk("%s: called()\n", __func__); }
+
+static void cas_rmt_q_monitor_policy_tx(struct rmt_ps *      ps,
+                                        struct pdu *         pdu,
+                                        struct rmt_n1_port * port)
+{
+        struct cas_rmt_queue * q;
+        ssize_t                cur_q;
+
+        ASSERT(ps);
+        ASSERT(ps->priv);
+        ASSERT(pdu);
+        ASSERT(port);
+
+        q = cas_rmt_queue_find(ps->priv, port->port_id);
+        if (!q) {
+                LOG_ERR("Monitoring: could not find CAS queue for N-1 port %u",
+                        port->port_id);
+                return;
+        }
+
+        cur_q    = rfifo_length(q->queue) + 1;
+        q->avg_q = ((q->n * q->avg_q + cur_q) / (q->n + 1));
+        q->n     = q->n + 1;
+
+        LOG_ERR("Average queue length for N-1 port %u just calculated: %u",
+                port->port_id, q->avg_q);
+
+        return;
+
+}
+
+static void cas_rmt_q_monitor_policy_rx(struct rmt_ps *      ps,
+                                        struct sdu *         sdu,
+                                        struct rmt_n1_port * port)
+{ printk("%s: called()\n", __func__); }
+
+static struct pdu *
+cas_rmt_next_scheduled_policy_tx(struct rmt_ps *      ps,
+                                      struct rmt_n1_port * port)
+{
+        printk("%s: called()\n", __func__);
+        return NULL;
+}
+
+static int cas_rmt_enqueue_scheduling_policy_tx(struct rmt_ps *      ps,
+                                                     struct rmt_n1_port * port,
+                                                     struct pdu *         pdu)
+{
+        printk("%s: called()\n", __func__);
+        return 0;
+}
+
+static int cas_rmt_scheduling_create_policy_tx(struct rmt_ps *      ps,
+                                                    struct rmt_n1_port * port)
+{ printk("%s: called()\n", __func__);
+  return 0;
+}
+
+static int cas_rmt_scheduling_destroy_policy_tx(struct rmt_ps *      ps,
+                                                     struct rmt_n1_port * port)
+{
+        printk("%s: called()\n", __func__);
+        return 0;
+}
+
+static int rmt_ps_set_policy_set_param(struct ps_base * bps,
+                                       const char    * name,
+                                       const char    * value)
+{
+        struct rmt_ps *ps = container_of(bps, struct rmt_ps, base);
+
+        (void) ps;
+
+        if (!name) {
+                LOG_ERR("Null parameter name");
+                return -1;
+        }
+
+        if (!value) {
+                LOG_ERR("Null parameter value");
+                return -1;
+        }
+
+        LOG_ERR("No such parameter to set");
+
+        return -1;
+}
+
+static struct ps_base *
+rmt_ps_cas_create(struct rina_component * component)
+{
+        struct rmt * rmt = rmt_from_component(component);
+        struct rmt_ps * ps = rkzalloc(sizeof(*ps), GFP_KERNEL);
+
+        if (!ps) {
+                return NULL;
+        }
+
+        ps->base.set_policy_set_param = rmt_ps_set_policy_set_param;
+        ps->dm = rmt;
+        ps->priv = NULL;
+        ps->max_q_policy_tx = cas_max_q_policy_tx;
+        ps->max_q_policy_rx = cas_max_q_policy_rx;
+        ps->rmt_q_monitor_policy_tx = cas_rmt_q_monitor_policy_tx;
+        ps->rmt_q_monitor_policy_rx = cas_rmt_q_monitor_policy_rx;
+        ps->rmt_next_scheduled_policy_tx     = cas_rmt_next_scheduled_policy_tx;
+        ps->rmt_enqueue_scheduling_policy_tx = cas_rmt_enqueue_scheduling_policy_tx;
+        ps->rmt_scheduling_create_policy_tx  = cas_rmt_scheduling_create_policy_tx;
+        ps->rmt_scheduling_destroy_policy_tx = cas_rmt_scheduling_destroy_policy_tx;
+
+        return &ps->base;
+}
+
+static void rmt_ps_cas_destroy(struct ps_base * bps)
+{
+        struct rmt_ps *ps = container_of(bps, struct rmt_ps, base);
+
+        if (bps) {
+                rkfree(ps);
+        }
+}
+
+struct ps_factory rmt_factory = {
+        .owner          = THIS_MODULE,
+        .create  = rmt_ps_cas_create,
+        .destroy = rmt_ps_cas_destroy,
+};
