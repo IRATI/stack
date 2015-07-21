1e1db080f5fae3bd06f5906f056b7f2e073e315b
Bernat Gaston <bernat.gaston@i2cat.net>
2015-07-21 16:23:31 +0200

    rinad addons cleaned debug log

--- rinad/src/ipcm/addons/ma/ribs/ipcp_obj.cc
+++ rinad/src/ipcm/addons/ma/ribs/ipcp_obj.cc
@@ -85,7 +85,6 @@ void IPCPObj::create_cb(const rina::rib::rib_handle_t rib,
 	(void)invoke_id;
 	(void)obj_reply;
 
-	LOG_DBG("[DEBUG] IEHAAAAA");
 	//Set return value
 	res.code_ = rina::cdap_rib::CDAP_SUCCESS;
 
