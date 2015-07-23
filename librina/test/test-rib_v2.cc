4e0b8154a46b76a5705f91f1ba0a30acfc164743
Marc Sune <marc.sune@bisdn.de>
2015-07-23 11:31:24 +0200

    Fixing tests
    Previous commit(s) changed open_connection signature and types.

--- librina/test/test-rib_v2.cc
+++ librina/test/test-rib_v2.cc
@@ -167,8 +167,8 @@ public:
 
 	virtual cdap_rib::con_handle_t remote_open_connection(
 			const cdap_rib::vers_info_t &ver,
-			const cdap_rib::src_info_t &src,
-			const cdap_rib::dest_info_t &dest,
+			const cdap_rib::ep_info_t &src,
+			const cdap_rib::ep_info_t &dest,
 			const cdap_rib::auth_policy &auth, int port_id){
 		 cdap_rib::con_handle_t con;
 		 return con;
