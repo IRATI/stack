f4e3b15cdf2c95fd173f3cd50942e123122d6a46
Dimitri Staessens <dimitri.staessens@ugent.be>
2015-09-11 11:48:37 +0200

    Refactor of kernel flow allocator
    linux:
    
    kfa.[c/h] : refactored code to comply with kernel guidelines

--- linux/net/rina/kfa.h
+++ linux/net/rina/kfa.h
@@ -12,7 +12,7 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
@@ -29,47 +29,47 @@
 
 struct kfa;
 
-struct kfa * kfa_create(void);
-int          kfa_destroy(struct kfa * instance);
+struct kfa *kfa_create(void);
+int	    kfa_destroy(struct kfa *instance);
 
 /* Only requests the pidm to reserve a valid port-id */
-port_id_t    kfa_port_id_reserve(struct kfa *     instance,
-                                 ipc_process_id_t id);
+port_id_t   kfa_port_id_reserve(struct kfa      *instance,
+				ipc_process_id_t id);
 
 /* Rquest the pidm to release a valid port-id */
-int          kfa_port_id_release(struct kfa * instance,
-                                 port_id_t    port_id);
+int	    kfa_port_id_release(struct kfa *instance,
+				port_id_t   port_id);
 
-int          kfa_flow_sdu_write(struct ipcp_instance_data * data,
-                                port_id_t     id,
-                                struct sdu *  sdu);
+int	    kfa_flow_sdu_write(struct ipcp_instance_data *data,
+			       port_id_t   id,
+			       struct sdu *sdu);
 
-int          kfa_flow_sdu_read(struct kfa *  instance,
-                               port_id_t     id,
-                               struct sdu ** sdu);
+int	    kfa_flow_sdu_read(struct kfa  *instance,
+			      port_id_t    id,
+			      struct sdu **sdu);
 
 #if 0
-struct ipcp_flow * kfa_flow_find_by_pid(struct kfa * instance,
-                                        port_id_t    pid);
+struct ipcp_flow *kfa_flow_find_by_pid(struct kfa *instance,
+				       port_id_t   pid);
 #endif /* 0 */
 
 struct rmt;
 
 /* structure automatically freed when there are no more readers or writers */
-int          kfa_flow_create(struct kfa *           instance,
-                             port_id_t              pid,
-                             struct ipcp_instance * ipcp);
+int	    kfa_flow_create(struct kfa           *instance,
+			    port_id_t		  pid,
+			    struct ipcp_instance *ipcp);
 
 /* set options associated with a flow e.g
  * int kfa_flow_opts_set(i, pid, FLOW_O_NONBLOCK)
  */
-int          kfa_flow_opts_set(struct kfa *  instance,
-			       port_id_t     pid,
-			       flow_opts_t   flow_opts);
+int	    kfa_flow_opts_set(struct kfa *instance,
+			      port_id_t   pid,
+			      flow_opts_t flow_opts);
 
 /* returns the current options for a flow */
-flow_opts_t  kfa_flow_opts(struct kfa *  instance,
-			   port_id_t     pid);
+flow_opts_t kfa_flow_opts(struct kfa *instance,
+			  port_id_t   pid);
 
-struct ipcp_instance * kfa_ipcp_instance(struct kfa * instance);
+struct ipcp_instance *kfa_ipcp_instance(struct kfa *instance);
 #endif /* RINA_KFA_H */
