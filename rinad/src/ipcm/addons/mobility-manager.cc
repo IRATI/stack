d315d0037e439aa2f42b42997f38903f1908a100
Eduard Grasa <eduard.grasa@i2cat.net>
2017-06-09 13:10:11 +0200

    rinad: fix bug in mobman

--- rinad/src/ipcm/addons/mobility-manager.cc
+++ rinad/src/ipcm/addons/mobility-manager.cc
@@ -620,8 +620,6 @@ void MobilityManager::execute_handover3(const rina::MediaReport& report)
 			return;
 		}
 
-		sleep.sleepForMili(1000);
-
 		//Enroll to the internet DIF
 		int_neigh_data.supportingDifName.processName = "mobile.DIF";
 		int_neigh_data.difName.processName = "internet.DIF";
@@ -743,7 +741,6 @@ void MobilityManager::execute_handover3(const rina::MediaReport& report)
 	}
 
 	if (hand_state.change_mob_dif) {
-		sleep.sleepForMili(1000);
 		int_neigh_data.supportingDifName.processName = next_mob_dif;
 		int_neigh_data.difName.processName = "internet.DIF";
 		if(IPCManager->enroll_to_dif(this, &promise, inet_ipcp->get_id(), int_neigh_data,
