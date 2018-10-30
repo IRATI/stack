de04a9cf8cf88ba45c5a7b4bf43d058f329e6b48
Miquel Tarzan <miquel.tarzan@i2cat.net>
2018-10-30 20:33:03 +0100

    Add debug logs

--- kernel/dtcp.c
+++ kernel/dtcp.c
@@ -337,6 +337,13 @@ static int populate_ctrl_pci(struct pci *  pci,
                 }
                 return 0;
         case PDU_TYPE_RENDEZVOUS:
+        {
+        	LOG_INFO("sndLWE: %u, sndRWE: %u, rcvLWE: %u, rcvRWE: %u",
+        			pci_control_my_left_wind_edge(pci),
+					pci_control_my_rt_wind_edge(pci),
+					pci_control_new_left_wind_edge(pci),
+					pci_control_new_rt_wind_edge(pci));
+        }
         case PDU_TYPE_CACK:
         	if (pci_control_last_seq_num_rcvd_set(pci, last_rcv_ctl_seq)) {
 			LOG_ERR("Could not set last ctrl sn rcvd");
@@ -525,7 +532,6 @@ static int rcv_flow_ctl(struct dtcp * dtcp,
     	if (dtcp->parent->rttq) {
     		rttq_drop(dtcp->parent->rttq, seq-credit);
     	}
-    	LOG_INFO("New RTT estimation: %u", dtcp->parent->sv->tr);
 
     	return update_window_and_rate(dtcp, du);
 }
