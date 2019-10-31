54d5833c4f7e5af34f8eefc3c6847ae5160b3bb9
edugrasa <eduard.grasa@i2cat.net>
2019-10-31 15:08:17 +0100

    kernel: rina-device: fix unsupported flag in kernels < 4.6.0

--- kernel/rina-device.c
+++ kernel/rina-device.c
@@ -154,7 +154,11 @@ static void rina_dev_setup(struct net_device *dev)
 	dev->flags = IFF_POINTOPOINT | IFF_NOARP | IFF_MULTICAST;
 	dev->priv_flags	|= IFF_LIVE_ADDR_CHANGE | IFF_NO_QUEUE
 		| IFF_DONT_BRIDGE
-		| IFF_PHONY_HEADROOM;
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4,6,0)
+		| IFF_DONT_BRIDGE | IFF_PHONY_HEADROOM;
+#else
+		| IFF_DONT_BRIDGE;
+#endif
 	netif_keep_dst(dev);
 	dev->features = NETIF_F_HW_CSUM;
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4,11,9)
